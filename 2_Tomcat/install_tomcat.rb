bd16e8d06f4b526eab87b317f0de82132307b44e
Jason Stableford <jstableford@jasons-imac.lan>
2018-08-09 11:08:31 -0400

    Changed tomcat service config to use local file.

--- 2_Tomcat/install_tomcat.rb
+++ 2_Tomcat/install_tomcat.rb
@@ -29,32 +29,10 @@ bash 'get_tomcat_binary' do
 	end
 
 systemd_unit 'tomcat.service' do
-	content "
-	[Unit]
-Description=Apache Tomcat Web Application Container
-After=syslog.target network.target
+	content IO.read('/home/vagrant/jstableford/recipes/tomcat.service')
+	action [:create, :enable, :start]
+end
 
-[Service]
-Type=forking
-
-Environment=JAVA_HOME=/usr/lib/jvm/jre
-Environment=CATALINA_PID=/opt/tomcat/temp/tomcat.pid
-Environment=CATALINA_HOME=/opt/tomcat
-Environment=CATALINA_BASE=/opt/tomcat
-Environment='CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC'
-Environment='JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom'
-
-ExecStart=/opt/tomcat/bin/startup.sh
-ExecStop=/bin/kill -15 $MAINPID
-
-User=tomcat
-Group=tomcat
-UMask=0007
-RestartSec=10
-Restart=always
-
-[Install]
-WantedBy=multi-user.target"
 action [:create, :enable, :start]
 end
 execute 'wait for Tomcat' do
